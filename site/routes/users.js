0e1e8aa72a0947245ea53715734d56f6931c3442
jhonnierandrey <jhonnierandrey@outlook.com>
2020-05-05 00:52:10 -0300

    Incio sección de registro de usuarios

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/usersController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Site under construction, please come back later :D');
 });
+
+router.use('/register', usersController.register)
+
 
 module.exports = router;
