4ff96d6e24f954f952359d3022519fa88a6bc171
jAntonio <ajhonatan27@gmail.com>
2020-05-22 12:13:34 -0300

    usando EJS en el home y el header

--- site/routes/index.js
+++ site/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Modas Emilse | Inicio' });
-
+  res.render('index', { title: 'Modas Emilse | Inicio' })});
 
 module.exports = router;
