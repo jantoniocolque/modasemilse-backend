24036ce2eed07b506d8a15056a3bf65e84b1c62a
jAntonio <ajhonatan27@gmail.com>
2020-05-29 13:39:29 -0300

    agregando la logica de busqueda de articulos basica

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,7 +1,20 @@
 const fs=require('fs');
 const path=require('path');
+
 const productsFilePath = path.join(__dirname,'../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath,'utf-8'));
+
+function removeDuplicates(originalArray, nameProperty) {
+    var newArray = [];
+    var objectProcess  = {};
+    for(var i=0; i<originalArray.length ; i++){
+        objectProcess[originalArray[i][nameProperty]] = originalArray[i];
+    }
+    for(var object in objectProcess) {
+        newArray.push(objectProcess[object]);
+    }
+     return newArray;
+}
 
 const controller = {
     root:(req,res)=>{
@@ -9,20 +22,23 @@ const controller = {
     },
     
     search:(req,res)=>{
-        const keywords=req.body.keywords.toLowerCase();
+        const keywords = req.query.keywords.toLowerCase();
         const arrayKeywords= keywords.split(' ');
         const findKeywords = [];
         for(var i=0; i<products.length ; i++)
         {
             for(var j=0; j<arrayKeywords.length; j++){
                 if(products[i].type.toLowerCase() == arrayKeywords[j]){
-                    findKeywords.push(products[i].id);
+                    findKeywords.push(products[i]);
                     j=arrayKeywords.length;
                 }
             }
         }
-        console.log(findKeywords);
-        res.send('Buscando');
+        res.render('tienda',{
+            title:'Tienda - Emilse',
+            titleContent: 'Resultados de busqueda',
+            products:removeDuplicates(findKeywords,'idArticle'),
+        });
     }
 }
 
--- site/routes/index.js
+++ site/routes/index.js
@@ -5,4 +5,6 @@ const indexController = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', indexController.root);
 
+/*GET search page*/
+router.get('/search',indexController.search);
 module.exports = router;
