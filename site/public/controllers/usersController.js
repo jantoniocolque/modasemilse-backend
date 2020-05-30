6bc705061ef69f26d4f0a5ee562504707b14d95e
jAntonio <ajhonatan27@gmail.com>
2020-05-30 00:15:30 -0300

    eliminando carpetas repetidas

--- site/public/controllers/usersController.js
+++
@@ -1,7 +0,0 @@
-let usersController = {
-    'register' : function(req, res) {
-        res.render('register', { title: 'Modas Emilse | Registro' });
-    },
-}
-
-module.exports = usersController;
--- site/public/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const indexController = require('../controllers/indexController');
-
-/* GET home page. */
-router.get('/', indexController.root);
-
-module.exports = router;
