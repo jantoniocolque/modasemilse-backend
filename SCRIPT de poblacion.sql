8577f36e50d27c0a875063fbcf51168de1f06dc2
Jaime <amalia-rosa.jaime@capgemini.com>
2020-07-13 00:33:22 -0300

    SCRIPT - Se agrega script para poblar tabla SIZES

--- SCRIPT de poblacion.sql
+++ SCRIPT de poblacion.sql
@@ -2,3 +2,12 @@
 
 INSERT INTO emilse_modas.roles VALUES (NULL, 'USER');
 INSERT INTO emilse_modas.roles VALUES (NULL, 'ADMIN');
+
+-- SCRIPT PARA TABLA SIZES --
+
+INSERT INTO emilse_modas.sizes VALUES (NULL, '1');
+INSERT INTO emilse_modas.sizes VALUES (NULL, '2');
+INSERT INTO emilse_modas.sizes VALUES (NULL, '3');
+INSERT INTO emilse_modas.sizes VALUES (NULL, '4');
+INSERT INTO emilse_modas.sizes VALUES (NULL, '5');
+INSERT INTO emilse_modas.sizes VALUES (NULL, '6');
