a28daa80e008c5db60a386be7109fd6940c57207
jhonnierandrey <jhonnierandrey@outlook.com>
2020-08-12 20:00:35 -0300

    Se remueve Middleware para la API de forma temporal

--- site/routes/api/users.js
+++ site/routes/api/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const usersController = require('../../controllers/api/usersController');
 const authenticationMiddleware = require('../../middleware/api/authentication');
 
-router.get('/',authenticationMiddleware, usersController.list);
+router.get('/', usersController.list);
 router.post('/login',usersController.login);
-router.get('/:id',authenticationMiddleware,usersController.find);
+router.get('/:id',usersController.find);
 
 module.exports = router;
