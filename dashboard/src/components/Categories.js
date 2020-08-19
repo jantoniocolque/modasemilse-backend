f093208da66b3b665f31915a008be3ae8ca0ffe2
jhonnierandrey <jhonnierandrey@outlook.com>
2020-08-19 01:38:44 -0300

    Categories ahora usa y lista las categorias presentes en la DB

--- dashboard/src/components/Categories.js
+++ dashboard/src/components/Categories.js
@@ -1,36 +1,86 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Category from './Category'
 
-function Categories(){
-    return(
-        <div className="card shadow mb-4">
+class Categories extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            categoriesOnDB : ""
+        }
+    }
+    
+    apiCall(url, consecuencia){
+        let store = JSON.parse(localStorage.getItem('login'));
+        fetch(url,{
+            method : 'GET',
+            headers : {'token':store.store},
+        })
+        .then( response => response.json())
+        .then( data => consecuencia(data))
+        .catch( e => console.log(e))
+    }
+    
+    componentDidMount(){
+        this.checkApiData();
+    }
+    
+    checkApiData(){
+        this.apiCall('http://localhost:3000/v1/products/', this.displayData);
+    }
+    
+    displayData = (data) => {
+        this.setState(
+            {
+                categoriesOnDB : data.meta.categorys
+            }
+            )
+        }
+
+    counter(toCount) {
+        var length = 0;
+        for( var key in toCount ) {
+            if( toCount.hasOwnProperty(key) ) {
+                ++length;
+            }
+        }
+        return length;
+    };
+    
+    render(){
+        let cat;
+        let allCategories = this.state.categoriesOnDB
+        //let categoriesTotal = this.counter(this.state.categoriesOnDB); -->not fully implemented yet
+
+        let categoriesAv = [];
+
+        for(cat in allCategories) {
+            categoriesAv.push(<Category bgcolor="bg-info" txtcolor="text-white" text={ cat } />)
+        }
+
+        if(categoriesAv.length < 3){
+            categoriesAv.push(
+                <div className="spinner-border text-info" role="status">
+                        <span className="sr-only"></span>
+                </div>
+            )
+        }
+
+        return (
+            <div className="card shadow mb-4">
             <div className="card-header py-3">
                 <h6 className="m-0 font-weight-bold text-primary">Categorías registradas</h6>
             </div>
             <div className="card-body">
                 <div className="row">
-                    <div className="col-lg-6 mb-4">
-                        <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 01" />
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                         <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 02" />
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                          <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 03" />
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                         <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 04" />
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 05" />
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <Category bgcolor="bg-info" txtcolor="text-white" text="Categoría 06" />
-                    </div>
+                        {
+                            categoriesAv.map((oneCat, i) => <div key={oneCat + i} className="col-lg-6 mb-4"> { oneCat } </div>)
+                        }
                 </div>
             </div>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Categories;
