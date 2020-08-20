38cc55e5a2cc0964af676d34c268371f03a45ee9
jhonnierandrey <jhonnierandrey@outlook.com>
2020-08-20 01:09:11 -0300

    Se implementa sistema de Ruteo y limitación de acceso sin Token

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// styles
+import './App.css';
+import './index.css'
+// -always available- elements
+import SideBar from './components/sideBar';
+import NavBar from './components/navBar';
+import Footer from './components/Footer';
+// views
+import App from './App'
+import Login from './login/login';
+import Products from './components/Views/Products'
+import Users from './components/Views/Users'
+import CategoriesResume from './components/Views/CategoriesResume';
+// view for 404
+import Notfound from './components/NotFound'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let siteNavigator;
+
+if (localStorage.getItem('login')){
+  siteNavigator = (
+    <Router>
+      <div id="wrapper">
+        <SideBar />
+          <div id="content">
+          <NavBar />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/products" component={Products} />
+              <Route path="/users" component={Users} />
+              <Route path="/categories" component={CategoriesResume} />
+              <Route component={Notfound} />
+            </Switch>
+          <Footer />
+          </div>
+      </div>
+    </Router>
+  )
+}else{
+  siteNavigator = (
+    <Router>
+      <div id="wrapper">
+          <div id="">
+            <Switch>
+              <Login />
+              <Route component={Notfound} />
+            </Switch>
+          <Footer />
+          </div>
+      </div>
+    </Router>
+  )
+}
+ReactDOM.render(siteNavigator, document.getElementById('root'))
