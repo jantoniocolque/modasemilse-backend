8eb8a9d57e9d655c896c49aa546ed612503dec12
jhonnierandrey <jhonnierandrey@outlook.com>
2020-08-20 01:10:12 -0300

    Se simplifica vista de Dashboard en App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,34 +1,13 @@
-import React from 'react';
-import './App.css';
-import SideBar from './components/sideBar';
-import NavBar from './components/navBar';
+import React, {Component} from 'react';
 import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
-//Pages
-import Login from './login/login';
-
-function App() {
-  return (
-    <div id="wrapper">
-      <Router>
-        <Switch>
-          <Route path="/" exact={true}>
-            <Login />
-          </Route>
-          <Route path="/home" exact={true}>
-            <SideBar />
-            <div id="content">
-              <NavBar />
-              <Dashboard />     
-              <Footer />
-            </div>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
-
+class App extends Component {
+  render(){
+    return (
+      <Dashboard />   
+      );
+    }
+  }
+  
 export default App;
+  
