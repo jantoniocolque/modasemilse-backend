6addff745a6bcecb3b6a35c8335336794fe32e8b
jhonnierandrey <jhonnierandrey@outlook.com>
2020-08-20 01:16:33 -0300

    Componentes para Tablas agregados

---
+++ dashboard/src/components/Views/tables/TableForProducts.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './Header';
+import Content from './Content';
+function Table(props){
+
+    const productsArray = Object.values(props.arrayInfo)
+
+    return(
+            <div>
+                <div className="card shadow mb-4">
+                    <div className="card-body">
+                        <div className="table-responsive">
+                            <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
+                                <Header mainField='ID' secondField='Nombre' thirdField='Descripción'/>
+                                <tbody>
+                                    { productsArray.map((item,i)=> <Content key={i} data={item} /> )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+			</div>
+    )
+}
+
+export default Table;
